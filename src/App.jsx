bfb127640f780ac2ea27769d3294787ccfb0894f
mattramis <121640749+mattramis@users.noreply.github.com>
2023-07-24 20:29:30 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
-import { puppyList } from "./data.js"; // Make sure the correct path is used for data.js
+import { puppyList } from "./data.js";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <div className="App">
       {puppies.map((puppy) => (
-        <p key={puppy.id}>{puppy.name}</p>
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
       ))}
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
